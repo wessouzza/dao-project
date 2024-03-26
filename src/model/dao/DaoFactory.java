bec154272c1e7662abf86c3d8e0acb76b561768f
wessouzza <wes-souza@hotmail.com>
2024-03-26 15:11:58 -0300

    Feat: DepartmentDAO

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,15 @@
 package model.dao;
 
 import db.DB;
+import model.dao.imp.DepartmentDaoJDBC;
 import model.dao.imp.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartementDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
